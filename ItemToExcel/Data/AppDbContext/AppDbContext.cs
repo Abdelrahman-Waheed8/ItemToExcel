691dafccf8485921293316a4ea525b2efadaa36e
Abdelrahman-Waheed8 <abdothenoobest@gmail.com>
2026-09-05 00:02:52 +0300

    Modified relationship of item to category and constraints on both tables using fluent api

--- ItemToExcel/Data/AppDbContext/AppDbContext.cs
+++ ItemToExcel/Data/AppDbContext/AppDbContext.cs
@@ -11,5 +11,30 @@ namespace ItemToExcel.Data.AppDbContext
 
         public DbSet<Item> Items { get; set; }
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Item>(i =>
+            {
+                i.HasKey(x => x.Id);
+                i.Property(x => x.Name).IsRequired().HasMaxLength(100);
+                i.Property(x => x.BeforeDiscount).IsRequired().HasPrecision(18,2);
+                i.Property(x => x.AfterDiscount).IsRequired().HasPrecision(18, 2);
+                i.HasOne(i => i.Category).WithMany(c => c.Items).HasForeignKey(i => i.CategoryId).IsRequired();
+
+                i.ToTable(t =>
+                {
+                    t.HasCheckConstraint("CK_Before_Discount", "[BeforeDiscount] > 0");
+                    t.HasCheckConstraint("CK_After_Discount", "[AfterDiscount] > 0");
+                    t.HasCheckConstraint("CK_Check_Valid_AfterDiscount", "[AfterDiscount] <= [BeforeDiscount]");
+                });
+            });
+
+            modelBuilder.Entity<Category>(c =>
+            {
+                c.HasKey(x => x.Id);
+                c.Property(x => x.Name).IsRequired().HasMaxLength(30);
+            });
+        }
     }
 }
